f53dbc727c013c317e488ba33168164dc14d55d5
steven1210 <814639416@qq.com>
2020-11-15 22:09:42 +0800

    demo

---
+++ Demo1/strType.py
@@ -0,0 +1,27 @@
+# int -- 整型
+num1 = 1
+print(type(num1))
+
+# float -- 浮点型
+num2 = 2.1
+print(type(num2))
+
+# str -- 字符型
+a = '你好'
+print(type(a))
+
+# tuple -- 元组
+b = (10, 20, 30, 40)
+print(type(b))
+
+# list -- 数组
+c = [10, 20, 30]
+print(type(c))
+
+# set -- 集合
+d = {10, 20, 30}
+print(type(d))
+
+# dict -- 字典
+e = {"name": "Tom", "age": 18}
+print(type(e))
