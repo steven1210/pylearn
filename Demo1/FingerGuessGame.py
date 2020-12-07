eea70188eb4b61f7b97bb8ebb7c0de990cf2bd98
steven1210 <814639416@qq.com>
2020-12-07 22:47:18 +0800

    猜拳游戏

--- Demo1/FingerGuessGame.py
+++ Demo1/FingerGuessGame.py
@@ -12,11 +12,13 @@ player = int(input('玩家请出拳：0--剪刀，1--石头，2--布：'))
 # 电脑：出拳
 computer = randint(0, 2)
 # 判断玩家获胜的几种方式
-if ((player == 0) and (computer == 2)) or ((player == 1) and (computer == 0)) or ((player == 2) and (computer == 1)):
+if ((player == 0) and (computer == 2)) or ((player == 1) and (computer == 0)) \
+        or ((player == 2) and (computer == 1)):
     print(f'玩家出拳：{player},电脑出拳：{computer}；玩家获胜！')
 # 判断电脑获胜的几种方式
-elif ((player == 0) and (computer == 1)) or ((player == 1) and (computer == 2)) or ((player == 2) and (computer == 0)):
+elif ((player == 0) and (computer == 1)) or ((player == 1) and (computer == 2)) \
+        or ((player == 2) and (computer == 0)):
     print(f'玩家出拳：{player},电脑出拳：{computer}; 电脑获胜！')
 # 判断平局
 else:
-    print(f'玩家出拳：{player},电脑出拳：{computer}; 平局')
+    print(f'玩家出拳：{player},电脑出拳：{computer}; 平局,别走，再来一次。')
